503fd2ed076c5e144e5b557e57e5e01f0ec0ce3a
Oliver <oliver.s.chen@gmail.com>
2023-05-13 17:52:07 -0700

    added 2d maze

--- main.py
+++ main.py
@@ -14,96 +14,66 @@ import pygame as pg
 import os
 # import settings 
 from settings import *
-from sprites import *
 # from pg.sprite import Sprite
 
 # set up assets folders
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
 
-# create game class in order to pass properties to the sprites file
+# use 2D array for the maze
+maze = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
-class Game:
-    def __init__(self):
-        # init game window etc.
-        pg.init()
-        pg.mixer.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption("my game")
-        self.clock = pg.time.Clock()
-        self.running = True
-        print(self.screen)
-    def new(self):
-        # starting a new game
-        self.score = 0
-        self.all_sprites = pg.sprite.Group()
-        self.platforms = pg.sprite.Group()
-        self.enemies = pg.sprite.Group()
-        self.player = Player(self)
-        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
-        # self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
-        self.all_sprites.add(self.plat1)
-        self.platforms.add(self.plat1)
-        
-        self.all_sprites.add(self.player)
-        for plat in PLATFORM_LIST:
-            p = Platform(*plat)
-            self.all_sprites.add(p)
-            self.platforms.add(p)
-        self.run()
-    def run(self):
-        self.playing = True
-        while self.playing:
-            self.clock.tick(FPS)
-            self.events()
-            self.update()
-            self.draw()
+# init pygame
+pg.init()
+
+# set the dimension of the screen that the maze will display on
+screen = pg.display.set_mode((1200,800))
+
+# set the name of the game window
+pg.display.set_caption("Oliver's Maze")
+
+# set the clock for the games frame rate
+clock = pg.time.Clock()
+
+# set the fps
+clock.tick(FPS)
+
+# drawing the cell
+def cell(row, col):
+    x = col * CELL_WIDTH
+    y = row * CELL_HEIGHT
+    if maze[row][col] == 1:
+        color = GREEN
+    else:
+        color = RED
+    # draw possible paths/walls on screen
+    pg.draw.rect(screen, color, [x, y, CELL_WIDTH, CELL_HEIGHT])
+
+# fill screen will color
+screen.fill(PURPLE)
+
+# this ensures main game loop will quit
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    # draw the maze
+    for row in range(len(maze)):
+        for col in range(len(maze[row])):
+            cell(row,col)
     
-    def events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                if self.playing:
-                    self.playing = False
-                self.running = False
-    def update(self):
-        self.all_sprites.update()
-        
-        # if the player is falling
-        if self.player.vel.y > 0:
-            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
-            if hits:
-                self.player.standing = True
-                if hits[0].variant == "normal":
-                    self.player.pos.y = hits[0].rect.top
-                    self.player.vel.y = 0
-            else:
-                self.player.standing = False
-
-
-    def draw(self):
-        self.screen.fill(GREEN)
-        self.all_sprites.draw(self.screen)
-        self.draw_text("SCORE: " + str(self.score), 40, WHITE, WIDTH/2, HEIGHT/2)
-        # is this a method or a function?
-        pg.display.flip()
-    def draw_text(self, text, size, color, x, y):
-        font_name = pg.font.match_font('arial')
-        font = pg.font.Font(font_name, size)
-        text_surface = font.render(text, True, color)
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (x,y)
-        self.screen.blit(text_surface, text_rect)
-    def get_mouse_now(self):
-        x,y = pg.mouse.get_pos()
-        return (x,y)
-    
-
-
-# instantiate the game class...
-g = Game()
-
-# kick off the game loop
-while g.running:
-    g.new()
+    pg.display.flip()
 
 pg.quit()
+
+        
